cf639f0d0ab552b281245bfeaa94aadba85e45f9
Bovtik <bovtik.work@gmail.com>
2016-06-05 15:17:05 +0300

    Initializing webSocket

--- app/server/server.js
+++ app/server/server.js
@@ -1,21 +1,30 @@
 var http = require('http'),
- express = require('express'),
- fs = require('fs'),
- path = require('path');
+		fs = require('fs'),
+ 		path = require('path');
 
-var server = express();
+var express = require('express');
+var app = express();
+var ws = require('socket.io')(1338);
 
-server.use('/browser', 
+app.use('/browser', 
   express.static(
     path.join(
       path.join(__dirname, '..'), 'browser')));
 
-server.get('/', function(req, res) {
-  var layout = fs.readFileSync('../browser/index.html', 'utf8');
+app.get('/', function(req, res) {
+  var layout = fs.readFileSync('app/browser/index.html', 'utf8');
   res.send(layout);
 });
 
-server.listen(1337, function () {
+app.listen(1337, function () {
   console.log('Listening on port 1337');
+});
+
+ws.on ('connection', function (socket) {
+	console.log('welcome');	
+	
+	socket.on ('disconnect', function (socket) {
+		console.log('goodbye');
+	});
 });
 
